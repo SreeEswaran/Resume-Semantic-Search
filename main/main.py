ae514b051f4aa8ae9bbccf958d4b34e845c6ae65
Sree Deekshitha Yerra <126445402+SreeEswaran@users.noreply.github.com>
2024-07-10 00:14:50 +0530

    Create main.py

---
+++ main/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, Query
+from embed import get_embedding
+from store_embed import client
+import numpy as np
+app = FastAPI()
+
+@app.get("/search")
+def search(query: str = Query(...)):
+    embedding = get_embedding(query)
+    search_result = client.search(
+        collection_name="resume",
+        query_vector=("text",embedding.tolist()),
+        limit=10  
+    )
+    return search_result
+    
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="localhost", port=3000)
