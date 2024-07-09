8b3d3b768977ad3a2b223f7167966b97138b1be2
Sree Deekshitha Yerra <126445402+SreeEswaran@users.noreply.github.com>
2024-07-09 23:27:56 +0530

    Create store_embed.py

---
+++ store_embed.py
@@ -0,0 +1,46 @@
+from qdrant_client import QdrantClient
+from qdrant_client.http.models import Distance, VectorParams,VectorStruct, PointStruct, MultiVectorConfig,MultiVectorComparator
+from embed import get_embeddings
+from dotenv import load_dotenv
+load_dotenv()
+import os
+import json
+import logging
+
+client = QdrantClient(url="https://cd543e04-34c7-4592-b68e-e0c6932f7d40.us-east4-0.gcp.cloud.qdrant.io",api_key=os.environ["api_key"])  # Use an in-memory database for testing
+
+def store_embeddings(embeddings, processed_resumes):
+    collection_name = "resume"
+    vector_name = "default"  # Ensure this matches the vector name used in Qdrant
+
+    logging.info(f"Storing embeddings for {len(embeddings)} resumes.")
+
+    # Check if the collection exists, if not create it
+    if not client.collection_exists(collection_name):
+        vector_params ={"text": VectorParams(size=384, distance=Distance.COSINE,multivector_config=MultiVectorConfig(
+            comparator=MultiVectorComparator.MAX_SIM
+        ),)}
+        logging.info(f"Creating collection '{collection_name}' with vector params: {vector_params}")
+        client.create_collection(
+            collection_name=collection_name,
+            vectors_config=vector_params
+        )
+
+    points = []
+    for i, embedding in enumerate(embeddings):
+        points.append(PointStruct(
+            id=i,
+            vector={"text":embedding.tolist()},
+            payload={"resume":processed_resumes[i]}
+        ))
+        
+
+    client.upsert(collection_name=collection_name, points=points)
+    print("Embeddings stored successfully.")
+
+
+with open('processed_resumes.json', 'r') as f:
+    processed_resumes = json.load(f)
+texts = [resume['text'] for resume in processed_resumes]
+embeddings = get_embeddings(texts)
+store_embeddings(embeddings, processed_resumes)
